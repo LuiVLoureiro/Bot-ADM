ec0f9af3b161d1315a94c93415d8ab6f7a090b01
LuiLoureiro <luivloureiro@gmail.com>
2024-04-14 10:24:07 -0300

    issue: Variavel de Ambiente

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import commands, tasks
 from datetime import datetime
 import asyncio
 import json
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -113,4 +114,4 @@ async def on_ready():
     print(f'Logged in as {client.user.name}')
     daily_task.start() 
 
-client.run(TOKEN_DISCORD)
+client.run(os.getenv('TOKEN_DISCORD'))
